c8b55298003135c1e136eec9a35dee5da2cfb742
Gero <gecam59@gmail.com>
2025-10-02 12:48:35 +0200

    Update to 1.21.9

--- src/main/java/de/gerrygames/mainhandsynchronization/mixin/PlayerMixin.java
+++ src/main/java/de/gerrygames/mainhandsynchronization/mixin/PlayerMixin.java
@@ -1,6 +1,7 @@
 package de.gerrygames.mainhandsynchronization.mixin;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Avatar;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,11 +9,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Player.class)
-public class PlayerMixin {
+@Mixin(Avatar.class)
+public abstract class PlayerMixin {
 
 	@Inject(method = "getMainArm", at = @At("RETURN"), cancellable = true)
 	public void getMainArm(CallbackInfoReturnable<HumanoidArm> cir) {
-		cir.setReturnValue(Minecraft.getInstance().options.mainHand().get());
+		if ((Object) this instanceof Player) {
+			cir.setReturnValue(Minecraft.getInstance().options.mainHand().get());
+		}
 	}
 }
